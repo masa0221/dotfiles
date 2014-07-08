d5719789a9a0f09d0e78f71a21ba1e0827ca500c
cw_tsuru <tsuru@chatwork.co.jp>
2014-07-08 19:37:10 +0900

    setup.sh追加

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DOT_FILES=(.zshrc .gitconfig .vimrc)
+
+for file in ${DOT_FILES[@]}; do 
+        ln -s $HOME/dotfiles/$file $HOME/$file
+done
+
+[ ! -d ~/.oh-my-zsh ] && git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
+[ ! -d ~/.vim/bundle ] && mkdir -p ~/.vim/bundle && git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim && vim -c ':NeoBundleInstall'
