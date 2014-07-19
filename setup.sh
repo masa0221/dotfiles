6594f5aba48caa0702867478af02a8b66db53ece
masa0221 <0221masa@gmail.com>
2014-07-19 18:40:59 +0900

    bugfix setup.sh

--- setup.sh
+++ setup.sh
@@ -3,17 +3,19 @@
 DOT_FILES=(.zshrc .gitconfig .vimrc)
 
 for file in ${DOT_FILES[@]}; do 
-    if [ -f $HOME/$file ] 
+    if [ ! -f $HOME/$file ]; then 
         ln -s $HOME/dotfiles/$file $HOME/$file
     fi
 done
 
-if [ ! -d ~/.oh-my-zsh ]
+if [ ! -d ~/.oh-my-zsh ]; then
     git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 fi
 
-if [ ! -d ~/.vim/bundle ]
+if [ ! -d ~/.vim/bundle ]; then
     mkdir -p ~/.vim/bundle
     git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
     vim -c ':NeoBundleInstall'
 fi
+
+echo 'Finished!'
