83c1f9c694357c4782c5772e3875d346e1fbc808
masa0221 <0221masa@gmail.com>
2014-08-14 02:49:11 +0900

    setup用ファイルを修正

--- setup.sh
+++ setup.sh
@@ -6,14 +6,19 @@ for file in ${DOT_FILES[@]}; do
     ln -fi $HOME/.dotfiles/$file $HOME/$file
 done
 
-if [ ! -d ~/.oh-my-zsh ]; then
-    git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-fi
-
 if [ ! -d ~/.vim/bundle ]; then
     mkdir -p ~/.vim/bundle
     git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
     vim -c ':NeoBundleInstall'
+fi
+
+echo "Install oh-my-zsh ? (y/n):"
+read answer
+
+if [ "${answer} = "Y" or "${answer} = "y" ]; then
+    if [ ! -d ~/.oh-my-zsh ]; then
+        git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+    fi
 fi
 
 echo 'Finished!'
