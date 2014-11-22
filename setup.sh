2a2a3e69193b4ced07e6d71052779484049f058f
masa0221 <0221masa@gmail.com>
2014-11-22 11:50:51 +0900

    bugfix

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ for DOT_FILE in ${DOT_FILES[@]}; do
 done
 
 if [ ! -d ~/.vim/bundle ]; then
-    mkdir -p ~/.vim/bundle
+    \mkdir -p ~/.vim/bundle
     git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
     vim -c ':NeoBundleInstall'
 fi
@@ -16,7 +16,7 @@ if [ ! -d ~/.oh-my-zsh ]; then
     echo "Do you want to install oh-my-zsh ? (y/n):"
     read ANSWER
 
-    if [ "${ANSWER} = "Y" or "${ANSWER} = "y" or "${ANSWER} = "yes" ]; then
+    if [ ${ANSWER} = "Y" or ${ANSWER} = "y" or ${ANSWER} = "yes" ]; then
         git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
     fi
 fi
