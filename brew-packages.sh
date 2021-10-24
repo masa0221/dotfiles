f436fa25749ba7117dac11e5820102b7c51aed52
Masashi Tsuru <0221masa@gmail.com>
2021-10-24 14:15:49 +0900

    Remove unnecessary packages

--- brew-packages.sh
+++ brew-packages.sh
@@ -4,35 +4,20 @@ brew update
 # Upgrade already-installed packages
 brew upgrade
 
-# Add repositories
-brew tap peco/peco
-brew tap homebrew/dupes
-
 # Install brew-packages
 brew install zsh
-brew install zplug
-brew install ansible
+brew install zinit
+brew install vim
+brew install tmux
+brew install fzf
 brew install autojump
-brew install git-flow
 brew install gnu-sed
-brew install peco
 brew install the_silver_searcher
 brew install tig
 brew install tree
-brew install vim
-brew install wget
-brew install tmux
 brew install awscli
-brew install ctags
-brew install mysql
-brew install reattach-to-user-namespace
-brew install anyenv
-brew install fzf
 brew install jq
-brew install kube-ps1
 
 # Remove outdated version
 brew cleanup
 
-# Install zplug plugins
-zplug install
